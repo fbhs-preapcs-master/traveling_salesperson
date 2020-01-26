6b056278a5166d48274702ef042403a969ad16fe
Chad Purdy <chpurdy@gmail.com>
2020-01-25 20:52:28 -0600

    completed visualization

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# traveling_salesperson
+# Traveling Salesperson Visualization
+
+* To setup, start by creating a virtual environment:
+
+    `python3 -m venv venv`
+
+* Then install the required packages:
+
+    `pip3 install -r requirements.txt`
+
+* Finally, run `traveling_salesperson.py`
+
+    The first time it will only have 3 points (cities)
+
+    Change the value of the constant `NUM_CITIES` to 4, then 5, then 6, etc. to see how the time to find the shortest path changes.  Do you notice a pattern? (Hint: What is the difference between 3! and 4!; between 4! and 5!; between 5! and 6!; etc.)
---
+++ traveling_salesperson.py
@@ -0,0 +1,85 @@
+import arcade
+from itertools import permutations
+from random import randint
+import math
+import time
+
+
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = 600
+SCREEN_TITLE = "Traveling Salesperson"
+NUM_CITIES = 3  # increase this one at a time to see what effect it has on how long it takes
+                # to find the shortest path
+
+class TravelingSales(arcade.Window):
+    def __init__(self, width, height, title):
+        super().__init__(width, height, title)
+
+        arcade.set_background_color(arcade.color.BLACK)
+
+
+    def setup(self):
+        self.cities = []
+        for i in range(NUM_CITIES):
+            city = arcade.Point(randint(0,SCREEN_WIDTH-1), randint(0,SCREEN_HEIGHT-1))
+            self.cities.append(city)
+        self.shortest = self.get_total_dist(self.cities)
+        self.permutations = list(permutations(self.cities))
+        self.current_permutation = 0
+        self.start_time = time.time()
+        self.done = False
+
+    def on_draw(self):
+        arcade.start_render()
+        arcade.draw_points(self.cities,arcade.color.WHITE,10)
+        arcade.draw_line_strip(self.cities, arcade.color.WHITE, 2)
+        percent_done = self.current_permutation / (len(self.permutations)-1) * 100
+        arcade.draw_text(f'{percent_done:.3f}%',SCREEN_WIDTH//2,30,arcade.color.WHITE,24,anchor_x='center')
+        arcade.draw_text(f"Shortest Path: {self.shortest:.3f}",SCREEN_WIDTH//2, SCREEN_HEIGHT-30, arcade.color.WHITE, anchor_x='center')
+        arcade.draw_text(f"Time to Find Shortest Path: {self.total_time:.3f} seconds", 
+                             SCREEN_WIDTH//2,                                                     
+                             SCREEN_HEIGHT - 60,
+                             arcade.color.WHITE,
+                             anchor_x='center')
+
+    def on_update(self, delta_time):
+        if self.current_permutation < len(self.permutations)-1:
+            self.current_permutation += 1
+            next_path = self.permutations[self.current_permutation]
+            dist = self.get_total_dist(next_path)
+            if dist < self.shortest:
+                self.shortest = dist
+                self.cities = next_path
+        elif not self.done:
+            self.total_time = time.time() - self.start_time
+            self.done = True
+
+
+    def distance_between_points(self, point1, point2):
+        ''' 
+        returns the linear distance between point1 and point2
+        '''
+        x1,y1 = point1
+        x2,y2 = point2
+        return math.sqrt((x2-x1)**2 + (y2-y1)**2)
+
+    def get_total_dist(self, points):
+        '''
+        returns the total distance of the current path
+        '''
+        total_dist = 0
+        for i in range(len(points)-1):
+            # add the distance from the current point to the next point
+            total_dist += self.distance_between_points(points[i], points[i+1])
+        return total_dist
+
+
+
+
+def main():
+    ts = TravelingSales(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+    ts.setup()
+    arcade.run()
+
+if __name__ == "__main__":
+    main()
